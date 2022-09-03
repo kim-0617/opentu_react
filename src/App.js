2385038fe007342a40b3809c4dc7be5c5d3da7a2
kim-0617 <kkk5993@naver.com>
2022-09-04 00:28:02 +0900

    22.09.04
    12:27

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,6 +21,35 @@ function App() {
       </header>
     </div>
   );
+}
+*/
+
+import React, { Component } from 'react';
+import Subject from './components/Subject'
+import TOC from './components/TOC'
+import Content from './components/Content'
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      Subject : {title : "Web", desc : "World Wide Web"},
+      Content : [
+        {id : 1, title : 'HTML', desc : "HTML is Markup..."},
+        {id : 2, title : 'CSS', desc : "CSS is design..."},
+        {id : 3, title : 'JAVASCRIPT', desc : "JAVASCRIPT is control..."},
+      ],
+    }
+  }
+  render() {
+    return (
+      <div className='App'>
+        <Subject title = {this.state.Subject.title} desc={this.state.Subject.desc}></Subject>
+        <TOC data = {this.state.Content}></TOC>
+        <Content title = {this.state.Content[0].title} desc={this.state.Content[0].desc}></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TOC.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class TOC extends Component {
+    render() {
+        const data = this.props.data;
+        const list = [];
+        for(let i = 0; i < data.length; i++){
+            list.push(<li key = {data[i].id}><a href={'content/' + data[i].id}>{data[i].title}</a></li>)
+        }
+        return (
+            <ul>
+                {list}
+            </ul>
+        );
+    }
+}
+
+export default TOC;
