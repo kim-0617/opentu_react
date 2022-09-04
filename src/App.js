b9d415029c7543f3413b4146939d0740589b7fd7
kim-0617 <kkk5993@naver.com>
2022-09-04 20:40:03 +0900

    22.09.04
    20:40

--- src/App.js
+++ src/App.js
@@ -33,6 +33,8 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      mode : 'read',
+      welcome : {title : "Welcome", desc : "Hello React App!"},
       Subject : {title : "Web", desc : "World Wide Web"},
       Content : [
         {id : 1, title : 'HTML', desc : "HTML is Markup..."},
@@ -42,11 +44,20 @@ class App extends Component {
     }
   }
   render() {
+    let _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }
+    else if (this.state.mode === 'read'){
+      _title = this.state.Content[0].title;
+      _desc = this.state.Content[0].desc;
+    }
     return (
       <div className='App'>
         <Subject title = {this.state.Subject.title} desc={this.state.Subject.desc}></Subject>
         <TOC data = {this.state.Content}></TOC>
-        <Content title = {this.state.Content[0].title} desc={this.state.Content[0].desc}></Content>
+        <Content title = {_title} desc={_desc}></Content>
       </div>
     );
   }
